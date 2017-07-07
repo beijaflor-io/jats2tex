faf2cbe86af50474be199ee1fb55143f538641bc
yamadapc <tacla.yamada@gmail.com>
2017-07-07 01:38:25 -0300

    Move examples around, ignore latin-1

--- src/Text/JaTex/Parser.hs
+++ src/Text/JaTex/Parser.hs
@@ -8,11 +8,15 @@ import           Text.XML.HXT.Core
 type JATSDoc = XmlTrees
 
 readJats :: Maybe String -> FilePath -> IO [XmlTree]
-readJats encoding fp = do
-    input <- CleanUp.readFile encoding fp
+readJats mencoding@(Just _) fp = do
+    input <- CleanUp.readFile mencoding fp
     parseJATS (Text.unpack input)
+readJats Nothing fp = do
+    input <- readFile fp
+    parseJATS input
 
 parseJATS :: String -> IO [XmlTree]
 parseJATS = runX . readString [ withValidate no
-                              , withInputEncoding utf8
+                              -- , withInputEncoding utf8
+                              , withSubstDTDEntities no
                               ]
