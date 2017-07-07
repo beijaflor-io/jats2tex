b841e3f82a6e02d2001b3e112164b37841ed1e5f
yamadapc <tacla.yamada@gmail.com>
2017-07-07 11:54:41 -0300

    Implement bootstrap password reset page

--- jats2tex-web/src/Foundation/Auth.hs
+++ jats2tex-web/src/Foundation/Auth.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE FlexibleContexts  #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes       #-}
+
 -- | Defines custom (bootstrap) handlers for Yesod.Auth.Email
 module Foundation.Auth where
 
@@ -12,51 +13,68 @@ import qualified Yesod.Auth.Message  as Msg
 import           Yesod.Core
 import           Yesod.Form
 
-data ForgotPasswordForm = ForgotPasswordForm { _forgotEmail :: Text }
-data PasswordForm = PasswordForm { _passwordCurrent :: Text, _passwordNew :: Text, _passwordConfirm :: Text }
-data UserForm = UserForm { _userFormEmail :: Text }
-data UserLoginForm = UserLoginForm { _loginEmail :: Text, _loginPassword :: Text }
+data ForgotPasswordForm = ForgotPasswordForm
+  { _forgotEmail :: Text
+  }
 
-bootstrapRegisterHandler :: YesodAuthEmail master => HandlerT Auth (HandlerT master IO) Html
+data PasswordForm = PasswordForm
+  { _passwordCurrent :: Text
+  , _passwordNew     :: Text
+  , _passwordConfirm :: Text
+  }
+
+data UserForm = UserForm
+  { _userFormEmail :: Text
+  }
+
+data UserLoginForm = UserLoginForm
+  { _loginEmail    :: Text
+  , _loginPassword :: Text
+  }
+
+bootstrapRegisterHandler
+  :: YesodAuthEmail master
+  => HandlerT Auth (HandlerT master IO) Html
 bootstrapRegisterHandler = do
-    (widget, enctype) <- lift $ generateFormPost registrationForm
-    toParentRoute <- getRouteToParent
-    lift $ authLayout $ do
-        setTitleI Msg.RegisterLong
-        [whamlet|
+  (widget, enctype) <- lift $ generateFormPost registrationForm
+  toParentRoute <- getRouteToParent
+  lift $
+    authLayout $ do
+      setTitleI Msg.RegisterLong
+      [whamlet|
             <p>_{Msg.EnterEmail}
             <form method="post" action="@{toParentRoute registerR}" enctype=#{enctype}>
                 <div id="registerForm">
                     ^{widget}
                 <button .btn>_{Msg.Register}
         |]
-    where
-        registrationForm extra = do
-            let emailSettings = FieldSettings {
-                fsLabel = SomeMessage Msg.Email,
-                fsTooltip = Nothing,
-                fsId = Just "email",
-                fsName = Just "email",
-                fsAttrs = [("autofocus", ""), ("class", "form-control")]
+  where
+    registrationForm extra = do
+      let emailSettings =
+            FieldSettings
+            { fsLabel = SomeMessage Msg.Email
+            , fsTooltip = Nothing
+            , fsId = Just "email"
+            , fsName = Just "email"
+            , fsAttrs = [("autofocus", ""), ("class", "form-control")]
             }
-
-            (emailRes, emailView) <- mreq emailField emailSettings Nothing
-
-            let userRes = UserForm <$> emailRes
-            let widget = do [whamlet|
+      (emailRes, emailView) <- mreq emailField emailSettings Nothing
+      let userRes = UserForm <$> emailRes
+      let widget =
+            [whamlet|
                     #{extra}
                     <div .form-group>
                       <label> ^{fvLabel emailView}
                       ^{fvInput emailView}
                 |]
+      return (userRes, widget)
 
-            return (userRes, widget)
-
-bootstrapEmailLoginHandler :: YesodAuthEmail master => (Route Auth -> Route master) -> WidgetT master IO ()
+bootstrapEmailLoginHandler
+  :: YesodAuthEmail master
+  => (Route Auth -> Route master) -> WidgetT master IO ()
 bootstrapEmailLoginHandler toParent = do
-        (widget, enctype) <- liftWidgetT $ generateFormPost loginForm
-
-        [whamlet|
+  (widget, enctype) <- liftWidgetT $ generateFormPost loginForm
+  [whamlet|
             <form method="post" action="@{toParent loginR}", enctype=#{enctype}>
                 <div id="emailLoginForm">
                     ^{widget}
@@ -69,67 +87,79 @@ bootstrapEmailLoginHandler toParent = do
         |]
   where
     loginForm extra = do
-
-        emailMsg <- renderMessage' Msg.Email
-        (emailRes, emailView) <- mreq emailField (emailSettings emailMsg) Nothing
-
-        passwordMsg <- renderMessage' Msg.Password
-        (passwordRes, passwordView) <- mreq passwordField (passwordSettings passwordMsg) Nothing
-
-        let userRes = UserLoginForm Control.Applicative.<$> emailRes
-                                    Control.Applicative.<*> passwordRes
-        let widget = do [whamlet|
+      emailMsg <- renderMessage' Msg.Email
+      (emailRes, emailView) <- mreq emailField (emailSettings emailMsg) Nothing
+      passwordMsg <- renderMessage' Msg.Password
+      (passwordRes, passwordView) <-
+        mreq passwordField (passwordSettings passwordMsg) Nothing
+      let userRes =
+            UserLoginForm Control.Applicative.<$> emailRes Control.Applicative.<*>
+            passwordRes
+      let widget =
+            [whamlet|
                 #{extra}
                 <div .form-group>
                     ^{fvInput emailView}
                 <div .form-group>
                     ^{fvInput passwordView}
             |]
-
-        return (userRes, widget)
-    emailSettings emailMsg = do
-        FieldSettings {
-            fsLabel = SomeMessage Msg.Email,
-            fsTooltip = Nothing,
-            fsId = Just "email",
-            fsName = Just "email",
-            fsAttrs = [("autofocus", ""), ("placeholder", emailMsg), ("class", "form-control")]
-        }
+      return (userRes, widget)
+    emailSettings emailMsg =
+      FieldSettings
+      { fsLabel = SomeMessage Msg.Email
+      , fsTooltip = Nothing
+      , fsId = Just "email"
+      , fsName = Just "email"
+      , fsAttrs =
+          [ ("autofocus", "")
+          , ("placeholder", emailMsg)
+          , ("class", "form-control")
+          ]
+      }
     passwordSettings passwordMsg =
-         FieldSettings {
-            fsLabel = SomeMessage Msg.Password,
-            fsTooltip = Nothing,
-            fsId = Just "password",
-            fsName = Just "password",
-            fsAttrs = [("placeholder", passwordMsg), ("class", "form-control")]
-        }
+      FieldSettings
+      { fsLabel = SomeMessage Msg.Password
+      , fsTooltip = Nothing
+      , fsId = Just "password"
+      , fsName = Just "password"
+      , fsAttrs = [("placeholder", passwordMsg), ("class", "form-control")]
+      }
     renderMessage' msg = do
-        langs <- languages
-        master <- getYesod
-        return $ renderAuthMessage master langs msg
+      langs <- languages
+      master <- getYesod
+      return $ renderAuthMessage master langs msg
 
-bootstrapSetPasswordHandler :: YesodAuthEmail master => Bool -> HandlerT Auth (HandlerT master IO) TypedContent
+bootstrapSetPasswordHandler
+  :: YesodAuthEmail master
+  => Bool -> HandlerT Auth (HandlerT master IO) TypedContent
 bootstrapSetPasswordHandler needOld = do
-    messageRender <- lift getMessageRender
-    toParent <- getRouteToParent
-    selectRep $ do
-        provideJsonMessage $ messageRender Msg.SetPass
-        provideRep $ lift $ authLayout $ do
-            (widget, enctype) <- liftWidgetT $ generateFormPost setPasswordForm
-            setTitleI Msg.SetPassTitle
-            [whamlet|
+  messageRender <- lift getMessageRender
+  toParent <- getRouteToParent
+  selectRep $ do
+    provideJsonMessage $ messageRender Msg.SetPass
+    provideRep $
+      lift $
+      authLayout $ do
+        (widget, enctype) <- liftWidgetT $ generateFormPost setPasswordForm
+        setTitleI Msg.SetPassTitle
+        [whamlet|
                 <h3>_{Msg.SetPass}
                 <form method="post" action="@{toParent setpassR}" enctype=#{enctype}>
                     ^{widget}
             |]
   where
     setPasswordForm extra = do
-        (currentPasswordRes, currentPasswordView) <- mreq passwordField currentPasswordSettings Nothing
-        (newPasswordRes, newPasswordView) <- mreq passwordField newPasswordSettings Nothing
-        (confirmPasswordRes, confirmPasswordView) <- mreq passwordField confirmPasswordSettings Nothing
-
-        let passwordFormRes = PasswordForm <$> currentPasswordRes <*> newPasswordRes <*> confirmPasswordRes
-        let widget = do [whamlet|
+      (currentPasswordRes, currentPasswordView) <-
+        mreq passwordField currentPasswordSettings Nothing
+      (newPasswordRes, newPasswordView) <-
+        mreq passwordField newPasswordSettings Nothing
+      (confirmPasswordRes, confirmPasswordView) <-
+        mreq passwordField confirmPasswordSettings Nothing
+      let passwordFormRes =
+            PasswordForm <$> currentPasswordRes <*> newPasswordRes <*>
+            confirmPasswordRes
+      let widget =
+            [whamlet|
                 #{extra}
 
                 $if needOld
@@ -145,40 +175,47 @@ bootstrapSetPasswordHandler needOld = do
                 <div .form-group>
                      <input .btn.btn-success type=submit value=_{Msg.SetPassTitle}>
             |]
-
-        return (passwordFormRes, widget)
+      return (passwordFormRes, widget)
     currentPasswordSettings =
-         FieldSettings {
-             fsLabel = SomeMessage Msg.CurrentPassword,
-             fsTooltip = Nothing,
-             fsId = Just "currentPassword",
-             fsName = Just "current",
-             fsAttrs = [("autofocus", ""), ("class", "form-control")]
-         }
+      FieldSettings
+      { fsLabel = SomeMessage Msg.CurrentPassword
+      , fsTooltip = Nothing
+      , fsId = Just "currentPassword"
+      , fsName = Just "current"
+      , fsAttrs = [("autofocus", ""), ("class", "form-control")]
+      }
     newPasswordSettings =
-        FieldSettings {
-            fsLabel = SomeMessage Msg.NewPass,
-            fsTooltip = Nothing,
-            fsId = Just "newPassword",
-            fsName = Just "new",
-            fsAttrs = [("autofocus", ""), (":not", ""), ("needOld:autofocus", ""), ("class", "form-control")]
-        }
+      FieldSettings
+      { fsLabel = SomeMessage Msg.NewPass
+      , fsTooltip = Nothing
+      , fsId = Just "newPassword"
+      , fsName = Just "new"
+      , fsAttrs =
+          [ ("autofocus", "")
+          , (":not", "")
+          , ("needOld:autofocus", "")
+          , ("class", "form-control")
+          ]
+      }
     confirmPasswordSettings =
-        FieldSettings {
-            fsLabel = SomeMessage Msg.ConfirmPass,
-            fsTooltip = Nothing,
-            fsId = Just "confirmPassword",
-            fsName = Just "confirm",
-            fsAttrs = [("autofocus", ""), ("class", "form-control")]
-        }
+      FieldSettings
+      { fsLabel = SomeMessage Msg.ConfirmPass
+      , fsTooltip = Nothing
+      , fsId = Just "confirmPassword"
+      , fsName = Just "confirm"
+      , fsAttrs = [("autofocus", ""), ("class", "form-control")]
+      }
 
-bootstrapForgotPasswordHandler :: YesodAuthEmail master => HandlerT Auth (HandlerT master IO) Html
+bootstrapForgotPasswordHandler
+  :: YesodAuthEmail master
+  => HandlerT Auth (HandlerT master IO) Html
 bootstrapForgotPasswordHandler = do
-    (widget, enctype) <- lift $ generateFormPost forgotPasswordForm
-    toParent <- getRouteToParent
-    lift $ authLayout $ do
-        setTitleI Msg.PasswordResetTitle
-        [whamlet|
+  (widget, enctype) <- lift $ generateFormPost forgotPasswordForm
+  toParent <- getRouteToParent
+  lift $
+    authLayout $ do
+      setTitleI Msg.PasswordResetTitle
+      [whamlet|
             <p>_{Msg.PasswordResetPrompt}
             <form method=post action=@{toParent forgotPasswordR} enctype=#{enctype}>
                 <div id="forgotPasswordForm">
@@ -187,21 +224,21 @@ bootstrapForgotPasswordHandler = do
         |]
   where
     forgotPasswordForm extra = do
-        (emailRes, emailView) <- mreq emailField emailSettings Nothing
-
-        let forgotPasswordRes = ForgotPasswordForm <$> emailRes
-        let widget = do [whamlet|
+      (emailRes, emailView) <- mreq emailField emailSettings Nothing
+      let forgotPasswordRes = ForgotPasswordForm <$> emailRes
+      let widget =
+            [whamlet|
                 #{extra}
-                ^{fvLabel emailView}
-                ^{fvInput emailView}
+                <div .form-group>
+                    <label> ^{fvLabel emailView}
+                    ^{fvInput emailView}
             |]
-        return (forgotPasswordRes, widget)
-
+      return (forgotPasswordRes, widget)
     emailSettings =
-        FieldSettings {
-            fsLabel = SomeMessage Msg.ProvideIdentifier,
-            fsTooltip = Nothing,
-            fsId = Just "forgotPassword",
-            fsName = Just "email",
-            fsAttrs = [("autofocus", "")]
-        }
+      FieldSettings
+      { fsLabel = SomeMessage Msg.ProvideIdentifier
+      , fsTooltip = Nothing
+      , fsId = Just "forgotPassword"
+      , fsName = Just "email"
+      , fsAttrs = [("autofocus", ""), ("class", "form-control")]
+      }
